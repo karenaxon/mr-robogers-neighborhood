fefe6883fb150ec502f0aa1ae52fbcece988ec7c
Kaxon <karenaxon@users.noreply.github.com>
2021-10-29 10:57:31 -0700

    Add second test to README and corresponding code to scripts.js.

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-## Tests
+## Tests / Specifications
 Describe: translator()
 
 Test: "It should return 0 if the input from the user is 0." 
 Code: translator(0);
 Expected Output: 0
 
+Test: "It should return 'Beep!' if the input from the user is 1." 
+Code: translator(1);
+Expected Output: "Beep!"
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 
 
 function translator(number) {
+  const robogersLanguage = ["Beep!", "Boop!", "Won't you be my neighbor?"];
+  const keyArray = [1, 2, 3];
+  let translation, index;
+
   if (number === 0) {
     return 0;
+  } else if (keyArray.includes(number)){
+    index = keyArray.indexOf(number);
+    return translation = robogersLanguage[index]; 
   }
-
+  
 }
 
 
@@ -12,14 +19,11 @@ $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     $("#output").hide();
-
     const originalNumber = parseInt($("input#number").val());
     let translation = translator(originalNumber);
     
-
     $("#number-translated").text(translation);
     $("#output").show();
-
   });
-
+  
 });
